77c64cd3caec643dc028d3045d689949f8d4e115
vatsa287 <i.mnshreevatsa@gmail.com>
2020-01-23 21:22:34 +0530

    branch:delete-comment

--- 30-Days-Of-Code/Day-1/Solution.cpp
+++ 30-Days-Of-Code/Day-1/Solution.cpp
@@ -9,25 +9,13 @@ int main() {
     double d = 4.0;
     string s = "HackerRank ";
 
-    
-    // Declare second integer, double, and String variables.
-    
-    // Read and save an integer, double, and String to your variables.
-    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
-    
-    // Print the sum of both integer variables on a new line.
-    
-    // Print the sum of the double variables on a new line.
-    
-    // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
    int i1;
 double d1;
 string s1;
 
 cin >> i1;
 cin >> d1;
-getchar();//cin.ignore();
+getchar();
 getline(cin,s1);
 
 s.append(s1);
