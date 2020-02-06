bb38f3f4c4ab2e260260dd2666ca3fcbb195a75e
vatsa287 <i.mnshreevatsa@gmail.com>
2020-02-06 16:02:21 +0530

    Easy Method

---
+++ Problem-Solving/Large-average/solution.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+class solutionNaive {
+    public static void main(String args[] ) throws Exception {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("enter no of test cases");
+       int T = sc.nextInt();
+       while(T>0)
+       {   System.out.println("Enter range i.e L and R");
+           long L = sc.nextInt();
+           long R = sc.nextInt();
+           long avg =0;
+           long n = 2; //L and R
+           avg = (L+R)/n;
+           System.out.println("The avg for "+L+" and "+R);
+           System.out.println(avg);
+           T--;
+       }
+
+    }
+}
+
