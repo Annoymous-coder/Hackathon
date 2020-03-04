cdf25c08879b46c6f34f838827b86e20adb8c240
vatsa287 <i.mnshreevatsa@gmail.com>
2020-03-04 21:22:06 +0530

    Implements main method

---
+++ LinkedList/Runner.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Runner {
+	public static void main(String args[])
+	{
+		LinkedList list = new LinkedList();
+		list.insert(2);
+		list.insert(5);
+		list.show();
+	}
+}
