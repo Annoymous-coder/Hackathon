20918d073e1ced49b1adb32acc4aebf3fb2c4360
Shreevatsa N <53152913+vatsa287@users.noreply.github.com>
2020-03-04 21:20:58 +0530

    Documentational Commits.

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,37 +1,37 @@
-package practice;
+package practice;   //custom package
 class LinkedList 
 {
 	
-	Node head;
+	Node head;                           //declare first node head of type NODE
 	
-	public void insert(int data)
+	public void insert(int data)         //method to insert data(int) at the end of the list
 	{
-		Node node = new Node();
+		Node node = new Node();      //object creation.Here 'node' is the node to be inserted.
 		node.data = data;
-		node.next = null;
+		node.next = null;            //not neccessary since the default value is null.
 		
 		
-		if(head==null)
+		if(head==null)		     //check if there is no node yet i.e list is empty.        
 		{
-			head = node;
+			head = node;	     //so the new node is the head node.
 		}
 		else
 		{
-			Node n = head;
-		  while(n.next!=null){
-			n=n.next;
+			Node n = head;       //n is the temporary node created for traversal purposes.
+		  while(n.next!=null){	     //go to the last node.
+			n=n.next;	
 		  }
-		n.next = node;
+		n.next = node;		     //link the last node to the new node.
 		}
 	}
-	public void show()
-	{
-		Node n = head;
-		while(n.next!=null){
+	public void show()		     //method to display the data in the list.
+	{			
+		Node n = head;		     //n is the temporary node and intialized to head. Since head is the first node.		
+		while(n.next!=null){	     //goes to the last node. But last node has n.next==null so it stops for last 2nd node.	
 			System.out.println(n.data);
 			n=n.next;
 		}
-		//System.out.println(n.data);
+		System.out.println(n.data);   //for above problem print the contents of last node manually.	
 	}
 	
 }
