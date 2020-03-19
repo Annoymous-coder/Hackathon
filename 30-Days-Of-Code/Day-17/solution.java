7034904d5d6aff12cfb7449e7e9f790f3a617e35
vatsa287 <i.mnshreevatsa@gmail.com>
2020-03-19 13:08:37 +0530

    Day-17 Java solution

---
+++ 30-Days-Of-Code/Day-17/solution.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math.*;
+
+
+class Calculator {
+
+    int power(int n, int p) throws Exception
+    {   
+      if(n<0 || p <0)
+      {
+          throw new IllegalArgumentException ("n and p should be non-negative");
+      }
+      else
+      {
+          return((int)Math.pow(n,p));
+      }
+
+    } 
+}
+class Solution{
+
+    public static void main(String[] args) {
+    
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+        
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) { 					//The exception is handled by the power function's if-part where exception is thrown with the message.
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
