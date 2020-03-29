35d9f567116c3ed8cc554603649f272a0361560b
vatsa287 <i.mnshreevatsa@gmail.com>
2020-03-29 19:23:33 +0530

    reverseLL

---
+++ LinkedList/reverserLL.java
@@ -0,0 +1,18 @@
+tatic SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+
+            SinglyLinkedListNode cur = head;
+            SinglyLinkedListNode prev = null;
+            SinglyLinkedListNode next = null;
+
+            while(cur!=null)
+            {
+                next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+                
+            }
+            head = prev;
+            return head;
+
+    }
