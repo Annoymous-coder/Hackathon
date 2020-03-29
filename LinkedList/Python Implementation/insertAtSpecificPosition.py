5bfa3f3dc21a566b0ee92b4b72c79e59d17d2fc6
vatsa287 <i.mnshreevatsa@gmail.com>
2020-03-29 17:33:32 +0530

    py soln:

---
+++ LinkedList/Python Implementation/insertAtSpecificPosition.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtPosition function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtPosition(head, data, position):
+        node = SinglyLinkedListNode(data)
+        if position is 0:
+            node.next = head
+            head = node
+        else:
+            temp = head
+            for i in range(0,position-1):
+                temp = temp.next
+            node.next = temp.next
+            temp.next = node
+        return head
+if __name__ == '__main__':
