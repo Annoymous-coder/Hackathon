88ec37d03cd4528ec7fd606f6c5e99bf3879f2df
vatsa287 <i.mnshreevatsa@gmail.com>
2020-03-31 17:26:28 +0530

    Loop Detection

---
+++ LinkedList/Python Implementation/DetectLoopInLL.py
@@ -0,0 +1,24 @@
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+A Node is defined as: 
+ 
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
+
+
+def has_cycle(head):
+    fastptr = head
+    slowptr = head
+    cur = head
+    while(cur is not None and cur.next is not None):
+        slowptr = slowptr.next
+        fastptr = fastptr.next.next
+        if slowptr == fastptr:
+            return True
+        else:
+            return False
+        cur = cur.next
