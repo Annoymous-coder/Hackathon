783cf3133ce3cc30ea24586bbb27c60146a8c3c5
vatsa287 <i.mnshreevatsa@gmail.com>
2020-04-06 20:46:09 +0530

    docs

--- Problem-Solving-External/Maximsum-SubArray-k-SlidingWindow/ReturnsTheIndexTooSlidingWindowApproach.java
+++ Problem-Solving-External/Maximsum-SubArray-k-SlidingWindow/ReturnsTheIndexTooSlidingWindowApproach.java
@@ -1,4 +1,4 @@
-package practice;
+package practice;
 import java.util.*;
 public class MaximumSubarrayWithWindowSizeK {
 
@@ -25,7 +25,7 @@ public class MaximumSubarrayWithWindowSizeK {
 		}
 		
 		
-		System.out.println(start+" "+end);
+		System.out.println("start pos = "+start+"\n"+"end pos = "+end);
 		
 		return maxNum;
 	}
@@ -34,8 +34,49 @@ public class MaximumSubarrayWithWindowSizeK {
 		Scanner sc = new Scanner(System.in);
 		MaximumSubarrayWithWindowSizeK obj = new MaximumSubarrayWithWindowSizeK();
 		int A[] = {1,2,3,4,5,6,7,8};
-		int k = 4;
+		int k = 1;
 		int maxNum = obj.getMax(A,k);
-		System.out.println(maxNum);
+		System.out.println("The maximum subarray is "+maxNum);
+	}
+}
+package practice;
+import java.util.*;
+public class MaximumSubarrayWithWindowSizeK {
+
+	public int getMax(int A[],int k)
+	{	
+		int maxNum = 0, wsum = 0;
+		int start = 0, end = 0;
+		
+		for(int i=0;i<k;i++)
+		{
+			wsum+=A[i];
+			start = 0;
+			end = k-1;
+		}
+		for(int j=k;j<A.length;j++)
+		{	
+			maxNum = wsum;
+			wsum = wsum - A[j-k] + A[j];
+			if(wsum > maxNum){
+				maxNum = wsum;
+				start = j-k+1;
+				end = j;
+			}
+		}
+		
+		
+		System.out.println("start pos = "+start+"\n"+"end pos = "+end);
+		
+		return maxNum;
+	}
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		MaximumSubarrayWithWindowSizeK obj = new MaximumSubarrayWithWindowSizeK();
+		int A[] = {1,2,3,4,5,6,7,8};
+		int k = 1;
+		int maxNum = obj.getMax(A,k);
+		System.out.println("The maximum subarray is "+maxNum);
 	}
 }
