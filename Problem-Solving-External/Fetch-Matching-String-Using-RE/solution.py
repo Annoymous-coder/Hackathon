c24ac594b4ee8783ad897afe6725c9175a1fa216
vatsa287 <i.mnshreevatsa@gmail.com>
2020-04-08 13:33:50 +0530

    Regular Expression Problem

---
+++ Problem-Solving-External/Fetch-Matching-String-Using-RE/solution.py
@@ -0,0 +1,20 @@
+import re
+
+file = open('test.txt')
+
+#line = file.read()
+#print(line)
+
+resList = []
+for line in file:
+	print "Checking in the string:",line
+	x = re.findall("^[A-Z].*[/^].*[0-9]$",line)
+
+	if(x):
+		print("This string gets matched to the conditions\n")
+		resList.append(x)
+	else:
+		print("This string gets matched to the conditions")	
+print "\nThe extracted strings are"			
+print(resList)
+file.close()
