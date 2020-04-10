f737453e7ac3c22d4903f8e5fcef6010171c30dc
vatsa287 <i.mnshreevatsa@gmail.com>
2020-04-10 16:09:36 +0530

    Leetcode:

---
+++ Leetcode/Count-Subarray-sum-equals-k/solution.java
@@ -0,0 +1,20 @@
+#LEETCODE 560
+Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        //traverse from start to end
+        for(int i=0;i<nums.length;i++)
+        {   int sum = 0;   //make sum to 0 everytime since its local sum
+            for(int j=i;j<nums.length;j++)
+            {			//loop starts from i instead of 0 to avoid repetetion
+                sum = sum + nums[j];	//add the sum on the go till the end is reached
+                if(sum == k)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
