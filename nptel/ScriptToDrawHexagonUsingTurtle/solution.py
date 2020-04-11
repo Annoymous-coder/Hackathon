1e07ae1d5f04840a6a58310307b4043afdbf334c
vatsa287 <i.mnshreevatsa@gmail.com>
2020-04-11 18:48:07 +0530

    turtle-python script

---
+++ nptel/ScriptToDrawHexagonUsingTurtle/solution.py
@@ -0,0 +1,14 @@
+#Write a python script using turtle for drawing a hexagon
+import turtle
+polygon = turtle.Turtle()
+num_sides = 6
+side_length = 70
+angle = 360.0 / num_sides
+
+for i in range(num_sides):
+
+    polygon.forward(side_length)
+
+    polygon.right(angle)
+
+turtle.done()
