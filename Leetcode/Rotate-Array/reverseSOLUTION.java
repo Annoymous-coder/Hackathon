90e509b0e9b234f73c6d23a5fcff57ddc9c01fa9
vatsa287 <i.mnshreevatsa@gmail.com>
2020-04-14 20:27:35 +0530

    right rotation

---
+++ Leetcode/Rotate-Array/reverseSOLUTION.java
@@ -0,0 +1,24 @@
+//This program is rotate right problem, left rotation can be found at Hackerrank/problem-solving
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        
+        k = k%nums.length; //Incase of k >= n, to avoid cornercase
+        
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);    
+    }
+    public int[] reverse(int nums[], int start, int end)
+    {
+        while(start < end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+        return nums;
+    }
+}
