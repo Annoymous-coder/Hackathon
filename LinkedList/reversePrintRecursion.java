38b2d9836695cd52ffa36384bd803307770cb48e
vatsa287 <i.mnshreevatsa@gmail.com>
2020-05-07 17:12:37 +0530

    Docs
    Signed-off-by: vatsa287 <i.mnshreevatsa@gmail.com>

--- LinkedList/reversePrintRecursion.java
+++ LinkedList/reversePrintRecursion.java
@@ -10,3 +10,8 @@ static void reversePrint(SinglyLinkedListNode head) {
             System.out.println(head.data);
         }
     }
+//THIS FUNCITON ONLY PRINTS IN REVERSE ORDER BUT DOES NOT ACTUALLY REVERSE IT.USES STACK FOR IMPLEMENTATION.
+//1->2->3->4->5
+//PUSH(HEAD.NEXT(1)) INTO STACK THEN 2 , 3 , 4 , 5
+//SO THE STACK ACTUALLY CONTAINS IN ORDER 5 , 4 ,3 , 2 1
+//TOP = 5 SO LIFO ORDER STACK DATA IS DE-POPULATED
