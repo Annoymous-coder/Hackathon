493c6adb5c2cda64703829954b6e145148f11833
vatsa287 <i.mnshreevatsa@gmail.com>
2020-05-11 13:23:15 +0530

    pattern2

---
+++ Pattern-Programming/Pattern2.java
@@ -0,0 +1,29 @@
+package PatternProgramming;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		
+	/*	
+		*****		n=5
+		****
+		***
+		**
+		*
+
+	*/	
+		
+		int n = 5;
+		
+		for(int i=n; i>=1; i--)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
