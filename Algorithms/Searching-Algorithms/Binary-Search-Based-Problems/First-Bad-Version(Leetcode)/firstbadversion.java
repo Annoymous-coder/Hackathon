c1feacc0ed82685cff4dff5275917b62813926ba
Shreevatsa N <I.mnshreevatsa@gmail.com>
2020-05-15 12:25:13 +0530

    First Bad Version (Similar to first occurence of a key)

--- Algorithms/Searching-Algorithms/Binary-Search-Based-Problems/First-Bad-Version(Leetcode)/firstbadversion.java
+++ Algorithms/Searching-Algorithms/Binary-Search-Based-Problems/First-Bad-Version(Leetcode)/firstbadversion.java
@@ -22,8 +22,10 @@ call isBadVersion(4) -> true
 Then 4 is the first bad version.
 */
 
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
+/* 
+The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); 
+*/
 
 
 
