d9a5c686e88cb9029f8a475fc82fbe760ee4f06d
Shreevatsa N <I.mnshreevatsa@gmail.com>
2020-05-23 00:54:00 +0530

    Reshape The Matrix

--- Leetcode/ReshapeTheMatrix/InPlaceSolution.java
+++ Leetcode/ReshapeTheMatrix/InPlaceSolution.java
@@ -29,6 +29,11 @@ class Solution {
     }
 }
 /*
-Instead of unnecessarily using the queue as in the brute force approach, we can keep putting the numbers in the resultant matrix directly while iterating over the given matrix in a row-by-row order. While putting the numbers in the resultant array, we fix a particular row and keep on incrementing the column numbers only till we reach the end of the required columns indicated by ccc. At this moment, we update the row index by incrementing it and reset the column index to start from 0 again. Thus, we can save the space consumed by the queue for storing the data that just needs to be copied into a new 
+Instead of unnecessarily using the queue as in the brute force approach,
+we can keep putting the numbers in the resultant matrix directly while iterating over the given matrix in a row-by-row order.
+While putting the numbers in the resultant array,
+we fix a particular row and keep on incrementing the column numbers only till we reach the end of the required columns indicated by c.
+At this moment, we update the row index by incrementing it and reset the column index to start from 0 again.
+Thus, we can save the space consumed by the queue for storing the data that just needs to be copied into a new 
 array.
 */
