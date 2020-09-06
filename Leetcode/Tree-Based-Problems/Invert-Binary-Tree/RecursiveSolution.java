05b7fb806e604c87894db1f07d702a66d82ec2b2
vatsa287 <i.mnshreevatsa@gmail.com>
2020-09-06 18:59:05 +0530

    Invert Binary Tree

--- Leetcode/Tree-Based-Problems/Invert-Binary-Tree/RecursiveSolution.java
+++ Leetcode/Tree-Based-Problems/Invert-Binary-Tree/RecursiveSolution.java
@@ -23,3 +23,25 @@ class Solution {
         
     }
 }
+
+
+// Solution 2
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        if(root == null)
+            return null;
+        if(root != null)
+        {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            if(root.left != null)
+                invertTree(root.left);
+            if(root.right != null)
+                invertTree(root.right);
+        }
+        return root;
+    }
+}
